fd3332ec0ca0b0605c79254fa7a6b8f43fc5a796
HBIbidunni <ayodunibidunni@gmail.com>
2022-12-09 20:54:03 -0800

    6 print numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main -prints all single digit numbers.
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
