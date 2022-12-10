1b040607a3ebdfd60cbf70e584b19ae7fab3cb2f
HBIbidunni <ayodunibidunni@gmail.com>
2022-12-09 19:12:15 -0800

    2-print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char lc;
+
+	for (lc = 'a'; lc <= 'z'; lc++);
+	{
+		putchar(lc);
+	}
+	putchar('\n');
+	return (0);
