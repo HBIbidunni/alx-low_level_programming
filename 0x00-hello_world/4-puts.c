955db36b1a21cca76f1b86e4955e126dab08109a
HBIbidunni <ayodunibidunni@gmail.com>
2022-12-09 18:35:27 -0800

    The last digit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
  * main -main block
- * Return: o
+ * return: (0)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle);
+	puts("\"Programming is like building a multilingual puzzle");
 	returns (0);
 }
