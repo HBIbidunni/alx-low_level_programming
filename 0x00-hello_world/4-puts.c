deefc3c39cc271969c2cd23bc7c1974ff5de7478
HBIbidunni <ayodunibidunni@gmail.com>
2022-12-12 06:46:24 -0800

    4 puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 /**
- * main -main block
- * return: (0)
+ * main - Print a line of code using puts
+ *
+ * Return: 0
  */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	returns (0);
+	return (0);
 }
