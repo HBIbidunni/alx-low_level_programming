9a8c890c438895ae4418a7e9133846da9cef56bf
HBIbidunni <ayodunibidunni@gmail.com>
2022-12-15 20:40:45 -0800

    0-isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -6,15 +6,14 @@
 */
 int _isupper(int c)
 {
-	if (c >= '65' && c <= '90')
- {
- return (1);
- }
- else
+if (c >= '65' && c <= '90')
 {
- return (0);
+return (1);
 }
-
+else
+{
+return (0);
+}
 }
 
 
