18426b501968c7bc47c88669d1eb848fd3e82071
HBIbidunni <ayodunibidunni@gmail.com>
2022-12-22 10:00:56 -0800

    All Tasks

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,55 +1,31 @@
-#include "main.h"
-
+#include main.h
 /**
- * cap_string - Capitalizes all words of a string.
- * @str: The string to be capitalized.
- * Return: A pointer to the changed string.
+ * cap_string - function that capitalizes first character of a word
+ * @str: string to capitalize
+ * Return: return the capitalized string
  */
-
 char *cap_string(char *str)
-
 {
-
 	int index = 0;
 
-	while (str[index])
-
+	while (str[++index])
 	{
-
 		while (!(str[index] >= 'a' && str[index] <= 'z'))
 			index++;
-
 		if (str[index - 1] == ' ' ||
-
-
 				str[index - 1] == '\t' ||
-
-				str[index - 1] == '\n' ||
-
-				str[index - 1] == ',' ||
-
-				str[index - 1] == ';' ||
-
-				str[index - 1] == '.' ||
-
-				str[index - 1] == '!' ||
-
-				str[index - 1] == '?' ||
-
-				str[index - 1] == '"' ||
-
-				str[index - 1] == '(' ||
-
-				str[index - 1] == ')' ||
-
-				str[index - 1] == '{' ||
-
-				str[index - 1] == '}' ||
-
-				index == 0)
-
-				str[index] -= 32;
-	index++;
+				  str[index - 1] == '\n' ||
+				    str[index - 1] == ',' ||
+				      str[index - 1] == ';' ||
+     			                str[index - 1] == '.' ||
+					  str[index - 1] == '!' ||
+					    str[index - 1] == '?' ||
+		   			      str[index - 1] == '"' ||
+           				        str[index - 1] == '(' ||
+						  str[index - 1] == ')' ||
+						    str[index - 1] == '{' ||
+						      str[index - 1] == '}')
+			str[index] -= 32;
 	}
 	return (str);
 }
--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,43 +1,23 @@
-#include "main.h"
-
+#include main.h
 /**
- * leet - encode into 1337speak
- * @n: input value
- * Return: n value
+ * leet - function that encode a string
+ * @str: string that will encode
+ * Return: returns encoded string
  */
 
-char *leet(char *n)
-
+char *leet(char *str)
 {
+	int index1 = 0, index2;
+	char leet[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};
 
-	int i, j;
-
-	char s1[] = "aAeEoOtTlL";
-
-	char s2[] = "4433007711";
-
-	for (i = 0; n[i] != '\0'; i++)
-
+	while (str[++index1])
 	{
-
-		for (j = 0; j < 10; j++)
-
+		for (index2 = 0; index2 <= 7; index2++)
 		{
-
-			if (n[i] == s1[j])
-
-			{
-
-				n[i] = s2[j];
-
-
-
-			}
-
+			if (str[index1] == leet[index2] ||
+					str[index1] - 32 == leet[index2])
+				str[index1] = index2 + '0';
 		}
-
-		return (n);
-
 	}
-
+	return (str);
 }
