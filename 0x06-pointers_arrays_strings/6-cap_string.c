d7cd17193e42e472f2666a156719a735ef0c3b05
HBIbidunni <ayodunibidunni@gmail.com>
2022-12-22 04:25:15 -0800

    No 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,21 +7,15 @@
  */
 
 char *cap_string(char *str)
-
 {
 
 	int index = 0;
 
-	while (str[index])
-
+	while (str[++index])
 	{
-
 		while (!(str[index] >= 'a' && str[index] <= 'z'))
-
 			index++;
-
 		if (str[index - 1] == ' ' ||
-
 				str[index - 1] == '\t' ||
 
 				str[index - 1] == '\n' ||
@@ -44,18 +38,10 @@ char *cap_string(char *str)
 
 				str[index - 1] == '{' ||
 
-				str[index - 1] == '}' ||
-
-				index == 0)
+				str[index - 1] == '}')
 
 				str[index] -= 32;
-
-		index++;
-
 	}
 
 	return (str);
-
-}
-
-
+}}
