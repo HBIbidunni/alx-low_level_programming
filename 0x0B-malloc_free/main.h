c59cf6cabc53339d1687031743a38c294bf810c1
HBIbidunni <ayodunibidunni@gmail.com>
2023-01-09 17:57:21 -0800

    Task 1

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,5 +1,9 @@
+
 #ifndef MAIN_H
+
 #define MAIN_H
+
+
 
 char *create_array(unsigned int size, char c);
 
@@ -15,7 +19,11 @@ void free_grid(int **grid, int height);
 
 char *argstostr(int ac, char **av);
 
-#endf
+
+
+#endif
+
+
 
 
 
