2b232a9281bbd0b980f2ad4534555909442b2ced
HBIbidunni <ayodunibidunni@gmail.com>
2023-03-18 15:13:15 -0700

    All 0x0C. C - More malloc, free Tasks

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,75 +1,48 @@
 #include <stdlib.h>
 
-#include "main.h"
 /**
- * _realloc - reallocates a memory block using malloc and free
- * @ptr: pointer to the memory previsouly allocated by malloc
- * @old_size: size of the allocated memory for ptr
- * @new_size: new size of the new memory block
+ * _realloc - reallocate a memory block using malloc and free
  *
- * Return: pointer to the newly allocated memory block
- */
+ * @ptr: pointer to previous memory
+ * @old_size: old memory array size
+ * @new_size: new memory array size
+ *
+ * Return: pointer to new memory
+ *         NULL if it fails
+*/
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
-
 {
-
-	char *ptr1;
-
-	char *old_ptr;
-
+	char *new_ptr, *temp_ptr;
 	unsigned int i;
 
 	if (new_size == old_size)
-
 		return (ptr);
 
-	if (new_size == 0 && ptr)
-
+	if (ptr == NULL)
 	{
-
+		new_ptr = malloc(new_size);
+		if (new_ptr == NULL)
+			return (NULL);
 		free(ptr);
+		return (new_ptr);
+	}
 
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	new_ptr = malloc(new_size);
+	if (new_ptr == NULL)
 		return (NULL);
 
-	}
+	temp_ptr = ptr;
 
-	if (!ptr)
-
-		return (malloc(new_size));
-
-	ptr1 = malloc(new_size);
-
-	if (!ptr1)
-
-		return (NULL);
-
-	old_ptr = ptr;
-
-	if (new_size < old_size)
-
-	{
-
-		for (i = 0; i < new_size; i++)
-
-			ptr1[i] = old_ptr[i];
-
-	}
-
-	if (new_size > old_size)
-
-	{
-
-		for (i = 0; i < old_size; i++)
-
-			ptr1[i] = old_ptr[i];
-
-	}
+	for (i = 0; i < old_size; i++)
+		new_ptr[i] = temp_ptr[i];
 
 	free(ptr);
-
-	return (ptr1);
-
+	return (new_ptr);
 }
-
-
