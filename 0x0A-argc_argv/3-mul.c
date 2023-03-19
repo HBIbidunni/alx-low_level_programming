ae7a3d91a2807c785e5420fda1a8a582c249e161
HBIbidunni <ayodunibidunni@gmail.com>
2023-03-18 17:37:20 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,32 +1,23 @@
-#include "main.h"                                                                                                                    
-#include <stdlib.h>                                                                                                                  
-#include <stdio.h>                                                                                                                   
-                                                                                                                                     
-/**                                                                                                                                  
- * main - prints the multiplication of two integers                                                                                  
- * @argc: argument count                                                                                                             
- * @argv: argument vector                                                                                                            
- * Return: 0 if true, 1 if false                                                                                                     
- */                                                                                                                                  
-int main(int argc, char *argv[])                                                                                                     
-{                                                                                                                                    
-int a, b;                                                                                                                            
-if (argc == 3)                                                                                                                       
-{                                                                                                                                    
-a = atoi(argv[1]);                                                                                                                   
-b = atoi(argv[2]);                                                                                                                   
-printf("%d\n", a *b);                                                                                                                
-return (0);                                                                                                                          
-}                                                                                                                                    
-printf("Error\n");                                                                                                                   
-return (1);                                                                                                                          
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiplies two numbers followed by a new line
+ * @argc: argument count
+ * @argv: arguments passed to main as an array of strings
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	if (argc == 3)
+	{
+		int  result = atoi(argv[1]) * atoi(argv[2]);
+
+		printf("%i\n", result);
+		return (0);
+	}
+	else
+		printf("Error\n");
+	return (1);
 }
-
-
-
-
-
-
-
-
-
