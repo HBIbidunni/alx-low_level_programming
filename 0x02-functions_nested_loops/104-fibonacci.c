84efb2c8e6487a27dcbb331bfcc465bba7a69117
HBIbidunni <ayodunibidunni@gmail.com>
2023-03-20 18:47:14 -0700

    all C - Functions, nested loops tasks

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,69 +1,73 @@
+#include "main.h"
 #include <stdio.h>
 
 /**
- * main - fibonacci <3
- * Purpose - no hardcode
- * Return: (Success)
- */
+ * numLength - returns the length of string
+ *
+ * @num: operand number
+ *
+ * Return: number of digits
+*/
+
+int numLength(int num)
+{
+	int length = 0;
+
+	if (!num)
+		return (1);
+
+	while (num)
+	{
+		num = num / 10;
+		length += 1;
+	}
+
+	return (length);
+}
+
+/**
+ * main - Entry point
+ *
+ * Description: prints the first 98 Fibonacci numbers
+ *	starting with 1 and 2 followed by a new line
+ *
+ *	Solution was copied from Nobert Patrick
+ *	Wise, github handle: Trikcode
+ *
+ * Return: Always 0 (Success)
+*/
+
 int main(void)
 {
+	int count, initial0s;
+	unsigned long f1 = 1, f2 = 2, sum, mx = 100000000, f1o = 0, f2o = 0, sumo = 0;
 
-	unsigned long int i;
-
-	unsigned long int bef = 1;
-
-	unsigned long int aft = 2;
-
-	unsigned long int l = 1000000000;
-
-	unsigned long int bef1;
-
-	unsigned long int bef2;
-
-	unsigned long int aft1;
-
-	unsigned long int aft2;
-
-	printf("%lu", bef);
-
-	for (i = 1; i < 91; i++)
-
+	for (count = 1; count <= 98; ++count)
 	{
+		if (f1o > 0)
+			printf("%lu", f1o);
+		initial0s = numLength(mx) - 1 - numLength(f1);
 
-		printf(", %lu", aft);
+		while (f1o > 0 && initial0s > 0)
+		{
+			printf("%d", 0);
+			--initial0s;
+		}
 
-		aft += bef;
+		printf("%lu", f1);
 
-		bef = aft - bef;
+		sum = (f1 + f2) % mx;
+		sumo = f1o + f2o + (f1 + f2) / mx;
+		f1 = f2;
+		f1o = f2o;
+		f2 = sum;
+		f2o = sumo;
 
+		if (count != 98)
+			printf(", ");
+		else
+			printf("\n");
 	}
-
-	bef1 = (bef / l);
-
-	bef2 = (bef % l);
-
-	aft1 = (aft / l);
-
-	aft2 = (aft % l);
-
-	for (i = 92; i < 99; ++i)
-	{
-		printf(", %lu", aft1 + (aft2 / l));
-
-		printf("%lu", aft2 % l);
-
-		aft1 = aft1 + bef1;
-
-		bef1 = aft1 - bef1;
-
-		aft2 = aft2 + bef2;
-
-		bef2 = aft2 - bef2;
-
-	}
-
-	printf("\n");
 
 	return (0);
-
 }
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,41 +1,30 @@
 #include "main.h"
 
-       /**
-	 * times_table - prints the 9 times table, starting with 0
-	 */
+/**
+ *times_table - Prints the 9 times table, starting with 0.
+ */
 void times_table(void)
+{
+	int num, mult, prod;
+
+	for (num = 0; num <= 9; num++)
 	{
-		int i, j, k;
+		_putchar('0');
 
-
-		for (i = 0; i < 10; i++)
+		for (mult = 1; mult <= 9; mult++)
 		{
-			for (j = 0; j < 10; j++)
-			{
-				k = j * i;
-				if (j == 0)
-				{
-					_putchar(k + '0');
-				}
+			_putchar(',');
+			_putchar(' ');
 
+			prod = num * mult;
 
-				if (k < 10 && j != 0)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(k + '0');
-				} else if (k >= 10)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar((k / 10) + '0');
-					_putchar((k % 10) + '0');
-				}
-			}
-			_putchar('\n');
+			if (prod <= 9)
+				_putchar(' ');
+			else
+				_putchar((prod / 10) + '0');
+
+			_putchar((prod % 10) + '0');
 		}
+		_putchar('\n');
 	}
-
-
-
+}
